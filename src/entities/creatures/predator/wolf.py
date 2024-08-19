5e4b02e542a74630afc9cdd01406412618b5a193
Vladislav Luchnikov <whatislov@mail.ru>
2024-08-19 17:53:03 +0300

    add logic attack wolf

--- src/entities/creatures/predator/wolf.py
+++ src/entities/creatures/predator/wolf.py
@@ -16,7 +16,7 @@ class Wolf(Predator):
     def make_move(self, map: Map) -> None:
         finder = WayFinder(map, self.get_food())
         way = finder.finding_shortest_way(self.get_coord())
-
+        print(way)
         if len(way) >= 1:
             self.migrate(way[1:], map)
 
@@ -27,8 +27,6 @@ class Wolf(Predator):
         else:
             steps = self.__cnt_cells_pass - 1
 
-        print(way)
-
         if self.is_attack_possible(way, steps):
             self.attack(way, steps, map)
         else:
@@ -36,7 +34,13 @@ class Wolf(Predator):
 
     def attack(self, way: list, steps: int, map: Map) -> None:
         herbivore_attacked = map.get_entity(way[-1])
-        print('АТАКА', self)
+
+        """if herbivore_attacked.get_hp() <= 0:
+            self.set_coord(herbivore_attacked.get_coord())
+        else:
+            herbivore_attacked.set_hp(herbivore_attacked.get_hp() - self._attack_power)"""
+
+        print('АТАКА', self, 'Сила: ', self._attack_power)
         print('Атакуемый:', herbivore_attacked)
 
     def is_attack_possible(self, way: list, steps: int) -> bool:
