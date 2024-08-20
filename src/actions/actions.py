6b57631543cd7d2f55ea80f87e5ecc887f5b30ec
Vladislav Luchnikov <whatislov@mail.ru>
2024-08-20 14:17:17 +0300

    add action_spawner

--- src/actions/actions.py
+++ src/actions/actions.py
@@ -1,17 +1,18 @@
 from src.entities.creatures.creature import Creature
 from src.map.map import Map
+from src.actions.action_spawner import ActionSpawner
 from src.reader import read_json
-from src.actions.actions_utils import ActionsUtils
 
 
 class Actions:
     def __init__(self, map: Map):
         self.__map = map
         self.__proportion = read_json('data/proportion.json')
+        self.__actions_spawner = ActionSpawner(self.__proportion, self.__map)
 
     def init_actions(self) -> None:
-        dict_entity = self.get_dict_entity(self.__proportion)
-        self.generate_entities(dict_entity)
+        dict_entity = self.__actions_spawner.get_dict_entity(self.__proportion)
+        self.__actions_spawner.generate_entities(dict_entity)
 
     def turn_actions(self) -> None:
         map = self.__map.get_map()
@@ -19,29 +20,6 @@ class Actions:
             if isinstance(entity, Creature):
                 entity.make_move(self.__map)
                 self.__map.update()
-        self.generate_entities(self.calc_cnt_missing_entity(self.__map.calc_entity_on_map()))
+        self.__actions_spawner.generate_entities(self.__actions_spawner.calc_cnt_missing_entity(self.__map.calc_entity_on_map()))
 
-    def calc_cnt_missing_entity(self, dict_entity_on_map: dict) -> dict:
-        dict_diff = {}
-        dict_entity_necessary = self.get_dict_entity(self.__proportion)
 
-        for i in dict_entity_necessary.keys():
-            if i in dict_entity_on_map.keys():
-                dict_diff[i] = dict_entity_necessary[i] - dict_entity_on_map[i]
-            else:
-                dict_diff[i] = dict_entity_necessary[i]
-
-        return dict_diff
-
-    def generate_entities(self, dict_entity: dict) -> None:
-        for entity, cnt_entity in dict_entity.items():
-            self.add_entity(entity, cnt_entity)
-
-    def get_dict_entity(self, proportion: dict) -> dict:
-        dict_entity_necessary = ActionsUtils().get_concrete_entities(proportion, self.__map.get_square_map())
-        return dict_entity_necessary
-
-    def add_entity(self, entity, count) -> None:
-        for _ in range(count):
-            coord = self.__map.get_random_empty_coord()
-            self.__map.add_entity_on_map(entity(coord), coord)
