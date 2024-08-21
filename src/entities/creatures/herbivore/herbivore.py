c9048fcf279c2cc874f6231ad171e1599b5e981f
Vladislav Luchnikov <whatislov@mail.ru>
2024-08-21 10:37:19 +0300

    remove logging

--- src/entities/creatures/herbivore/herbivore.py
+++ src/entities/creatures/herbivore/herbivore.py
@@ -15,7 +15,6 @@ class Herbivore(Creature):
             steps = len(way) - 1
         else:
             steps = self._cnt_cells_pass - 1
-        print(f'{self} переместился на {way[steps]}')
         self.set_coord(way[steps])
 
 
